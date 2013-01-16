52ba3fc4184cb65f67ff11d7481db8dd3a917e42
Brian Piltin <blpiltin@gmail.com>
2013-01-15 18:27:22 -0600

    Refactor Sign Up Tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,31 @@
 include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_title do |title|
+  match do |page|
+    page.should have_selector('title', text: title)
+  end
+end
+
+RSpec::Matchers.define :have_heading do |heading|
+  match do |page|
+    page.should have_selector('h1', text: heading)
+  end
+end
+
+RSpec::Matchers.define :have_error do
+  match do |page|
+		page.should_not have_selector('div.alert.alert-error')
+  end
+end
