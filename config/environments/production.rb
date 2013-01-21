02faf23a40391598644b43c6e67b6dd8ecb431c1
Brian Piltin <blpiltin@gmail.com>
2013-01-21 16:59:01 -0600

    Update name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-SampleApp::Application.configure do
+Appcontraption::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '98e7e65af0b2d03df37b9ad53d9def9c9cbe8cc7c33106e34c5493fa800cae56248dfabd0bf5febada0198bb873e4f52405bb2db5b3a3765cbc728f14e70428f'
+Appcontraption::Application.config.secret_token = '98e7e65af0b2d03df37b9ad53d9def9c9cbe8cc7c33106e34c5493fa800cae56248dfabd0bf5febada0198bb873e4f52405bb2db5b3a3765cbc728f14e70428f'
