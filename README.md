84fcc0184c3af72c30f4722df91f1418bb1e075e
Brian Piltin <blpiltin@gmail.com>
2013-01-21 20:19:19 -0600

    Update name

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Ruby on Rails Tutorial: sample application
+# App Contraption
 
-This is the sample application for
-[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
-by [Michael Hartl](http://michaelhartl.com/).
+This application allows users to modify aspects of their phonegap apps.
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-    let(:heading)    { 'Sample App' }
+    let(:heading)    { 'appcontraption' }
     let(:page_title) { '' }
 
     it_should_behave_like "all static pages"
@@ -104,7 +104,7 @@ describe "Static pages" do
     page.should have_title full_title('')
     click_link "Sign up now!"
     page.should have_title full_title('Sign up')
-    click_link "sample app"
+    click_link "appcontraption"
     page.should have_title full_title('')
   end
 
