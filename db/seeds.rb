b18c2824434b640ef7f0c71b84c600e6d97e0428
Brian Piltin <blpiltin@gmail.com>
2013-02-04 22:13:40 -0600

    Add db seed functions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def seed_users
+	admin = User.create!(
+		name: "Brian L. Piltin",
+		email: "blpiltin@gmail.com",
+		password: "jesus123",
+		password_confirmation: "jesus123")
+	admin.toggle!(:admin)
+end
+
+def seed_app_types
+  AppType.create! name: 'Restaurant'
+  # AppType.create! name: 'Sports Bar'
+  # AppType.create! name: 'Coffeeshop'
+  # AppType.create! name: 'Bookstore'
+end
+
+def seed_gadget_types
+  GadgetType.create! name: 'Menu'
+  # GadgetType.create! name: 'Events'
+  # GadgetType.create! name: 'Photos'
+  # GadgetType.create! name: 'Music'
+  # GadgetType.create! name: 'Contact'
+  # GadgetType.create! name: 'HomePage'
+end
+
+seed_users
+seed_app_types
+seed_gadget_types
